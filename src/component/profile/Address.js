3e0c8f27bb7995b4a5b8d98eb13ca120a417f454
rafik mami <rafik.mami94@gmail.com>
2021-01-29 17:01:37 +0100

    component

---
+++ src/component/profile/Address.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <>
+      <h2>Rue 1003 bouchemma gabes tunisia</h2>
+    </>
+  );
+};
+export default Address;
---
+++ src/component/profile/FullName.js
@@ -0,0 +1,11 @@
+import { getDefaultNormalizer } from "@testing-library/react";
+import React from "react";
+
+const FullName = () => {
+  return (
+    <>
+      <h1 className="name">Rafik mami</h1>
+    </>
+  );
+};
+export default FullName;
